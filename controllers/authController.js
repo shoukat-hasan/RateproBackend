2d1b980d343516eb6e54b4fe3bc07cbd04e42a28
Muhammad Shoukat Hasan <shoukat.hasan@gmail.com>
2025-08-06 08:48:16 +0500

    update login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -306,14 +306,17 @@ exports.loginUser = async (req, res, next) => {
         const { email, password, source = "public" } = req.body;
 
         const user = await User.findOne({ email });
-        
+
         // 👤 User not found
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
 
-        if (!user || !(await bcrypt.compare(password, user.password)))
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
             return res.status(401).json({ message: "Invalid email or password" });
+        }
 
         const isSystemAdmin = user.role === "admin" && user.email === "admin@ratepro.com";
 
