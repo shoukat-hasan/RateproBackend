68218db77ae9de0e23c042cc3928837afb087ea6
Muhammad Shoukat Hasan <shoukat.hasan@gmail.com>
2025-10-03 08:27:08 +0500

    remove cors optional

--- server.js
+++ server.js
@@ -52,9 +52,6 @@ app.use(
   })
 );
 
-// Handle preflight globally
-app.options("*", cors());
-
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
